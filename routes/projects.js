aa00c2a3c5befc894b57591bee8a1201426831b3
Ferhat Avdic <ferhat.avdic@gmail.com>
2017-09-05 00:04:34 +0200

    upload

--- routes/projects.js
+++ routes/projects.js
@@ -167,7 +167,7 @@ router.route('/projects/:projectId')
 //FILE UPLOAD
 var storage = multer.diskStorage({ //multers disk storage settings
         destination: function (req, file, cb) {
-            cb(null, '/uploads/');
+            cb(null, 'uploads/');
         },
         filename: function (req, file, cb) {
             var datetimestamp = Date.now();
