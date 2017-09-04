3ed46eef43f56243299a251621de23f7a0dc2abf
Ferhat Avdic <ferhat.avdic@gmail.com>
2017-09-04 23:05:41 +0200

    mongo connection

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/bsodb',
+  //database: 'mongodb://localhost:27017/bsodb',
+  database:'mongodb://heroku_6tn5jpnh:pg80ngv1imnk75o7blrimapcjk@ds125774.mlab.com:25774/heroku_6tn5jpnh',
   secret: 'yoursecret'
 }
